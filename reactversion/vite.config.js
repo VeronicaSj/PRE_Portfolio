e1f94726ca6b48be1ca38856afe40c04945b0c18
VeronicaSj <veronicasanchezjusticia2@gmail.com>
2025-09-07 01:05:26 +0200

    react migration test2

--- reactversion/vite.config.js
+++ reactversion/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-   base: 'https://github.com/VeronicaSj/PRE_Portfolio', // Link page
+   base: 'https://veronicasj.github.io/PRE_Portfolio/reactversion/', // Link page
 })
