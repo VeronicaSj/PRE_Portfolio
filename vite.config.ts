c39bc0700b2a78324e810a19104c6dfbf4d78dc4
Verónica Sánchez <veronicasanchezjusticia2@gmail.com>
2025-09-07 14:24:36 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/portfolio/', // <-- importante para Pages
+  base: '/PRE_Portfolio/', // <-- importante para Pages
 })
