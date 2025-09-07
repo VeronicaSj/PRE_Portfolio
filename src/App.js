a969d0d848dd09ee468c7ad33fc76c023cac12d1
VeronicaSj <veronicasanchezjusticia2@gmail.com>
2025-09-07 19:23:09 +0200

    no idea what im doing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import fullundividedpage from './components/fullundividedpage';
+
 function App() {
   return (
     <main style={{ fontFamily: "sans-serif", padding: "2rem" }}>
       <h1>Veronica's Portfolio</h1>
+      <fullundividedpage />
       <p>Coming soon...</p>
+      <fullundividedpage />
     </main>
+    
   );
 }
 
